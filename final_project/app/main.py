219f70b83d0b486b38b2370dd542202231108be3
svterskov <69151041+svterskov@users.noreply.github.com>
2024-11-03 14:57:37 +0300

    Update main.py

--- final_project/app/main.py
+++ final_project/app/main.py
@@ -1,1 +1,16 @@
+from fastapi import FastAPI
 
+from proper import fibonacci_proper
+from fallback import fibonacci_fallback
+
+app = FastAPI()
+
+@app.get("/fibonacci/{number}")
+async def read_fibonacci(number: int):
+
+    try:
+        result = fibonacci_proper(number)
+    except Exception as e:
+        result = fibonacci_fallback(number)
+        
+    return ({"number": number, "fibonacci": result})
